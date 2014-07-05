4289cdbceeffa9f5138248f001db8247c7ba47ec
Edgard Castro <edgardcastro@gmail.com>
2014-07-05 17:19:07 -0300

    Add mimeapps defaults

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ cat profile >> ~/.profile
 mkdir -p ~/.ssh && cp -f ssh_config ~/.ssh/config
 cp -f vimrc ~/.vimrc
 cp -f gtkrc-2.0 ~/.gtkrc-2.0
+mkdir -p ~/.local/share/applications && cat mimeapps.list >> ~/.local/share/applications/
