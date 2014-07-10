367633e101d0c17b63999e2f974b0f9f0e18be9b
Edgard Castro <edgardcastro@gmail.com>
2014-07-10 16:58:15 -0300

    Remove mimeapps

--- install.sh
+++ install.sh
@@ -5,4 +5,3 @@ cat profile >> ~/.profile
 mkdir -p ~/.ssh && cp -f ssh_config ~/.ssh/config
 cp -f vimrc ~/.vimrc
 cp -f gtkrc-2.0 ~/.gtkrc-2.0
-mkdir -p ~/.local/share/applications && cat mimeapps.list >> ~/.local/share/applications/
