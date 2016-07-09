0689a30afefc64ed1d8b486fafe12948746b30e2
Edgard Castro <castro@edgard.org>
2016-07-09 13:41:36 -0300

    lnx: remove thunderbird from dash and add spotify

--- bin/config-ubuntu.sh
+++ bin/config-ubuntu.sh
@@ -22,7 +22,7 @@ gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "'s
 gsettings set org.gnome.shell.overrides dynamic-workspaces false
 gsettings set org.gnome.system.locale region "'en_US.UTF-8'"
 gsettings set org.gnome.system.location enabled true
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'gnome-terminal.desktop', 'code.desktop', 'chrome-hmjkmjkepdijhoojdojkdfohbdgmmhki-Default.desktop', 'thunderbird.desktop', 'telegram.desktop', 'google-chrome.desktop']"
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'gnome-terminal.desktop', 'code.desktop', 'spotify.desktop', 'chrome-hmjkmjkepdijhoojdojkdfohbdgmmhki-Default.desktop', 'telegram.desktop', 'google-chrome.desktop']"
 
 # ext: caffeine
 GSETTINGS_SCHEMA_DIR=~/.local/share/gnome-shell/extensions/caffeine@patapon.info/schemas gsettings set org.gnome.shell.extensions.caffeine show-notifications false
