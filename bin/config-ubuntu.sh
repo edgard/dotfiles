f0295986c5e213c550f753d8a46ce4b0d2012489
Edgard Castro <castro@edgard.org>
2016-07-11 04:58:20 -0300

    lnx: run exectuables on click on gnome

--- bin/config-ubuntu.sh
+++ bin/config-ubuntu.sh
@@ -25,7 +25,7 @@ gsettings set org.gnome.system.location enabled true
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'gnome-terminal.desktop', 'code.desktop', 'spotify.desktop', 'chrome-hmjkmjkepdijhoojdojkdfohbdgmmhki-Default.desktop', 'telegram.desktop', 'google-chrome.desktop']"
 gsettings set org.gnome.nautilus.preferences sort-directories-first true
 gsettings set org.gtk.Settings.FileChooser sort-directories-first true
-
+gsettings set org.gnome.nautilus.preferences executable-text-activation "'launch'"
 
 # ext: caffeine
 GSETTINGS_SCHEMA_DIR=~/.local/share/gnome-shell/extensions/caffeine@patapon.info/schemas gsettings set org.gnome.shell.extensions.caffeine show-notifications false
