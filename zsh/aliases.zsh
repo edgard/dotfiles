3e8a82c67edb8260a9c948b08f89b83fbb679223
Edgard Castro <castro@edgard.org>
2016-09-27 04:40:35 -0300

    better update alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,5 +3,23 @@ alias j='z'
 alias history='history 1'
 alias ls='ls -FGh --color=auto'
 alias grep='grep --color --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=.git --exclude-dir=.bzr --exclude-dir=CVS --binary-files=without-match'
-alias update='pacaur -Syyu; sudo pacman -Rns $(pacman -Qtdq); (cd /opt/hardcode-fixer && git pull); sudo /opt/hardcode-fixer/fix.sh; (cd /opt/Hardcode-Tray && git pull); /opt/Hardcode-Tray/hardcode-tray --apply; (cd /opt/numix-folders && git pull); sudo /opt/numix-folders/numix-folders -p; rm -f ~/.local/share/applications/telegramdesktop.desktop; sudo chown -R edgard.users /opt/{hardcode-fixer,Hardcode-Tray,numix-folders}'
 alias open='xdg-open &> /dev/null'
+
+update() {
+    pacaur -Syyu && sudo pacman -Rns $(pacman -Qtdq)
+
+    opt_repos="hardcode-fixer,Hardcode-Tray,numix-folders"
+    current_user=${USER}
+    current_group=$(id -gn)
+    for repo in ${opt_repos//,/ }; do
+        echo "Updating opt repo ${repo}..."
+        (cd "/opt/${repo}" && git pull)
+        chown -R "${current_user}"."${current_group}" "/opt/${repo}"
+    done
+
+    sudo /opt/hardcode-fixer/fix.sh
+    /opt/Hardcode-Tray/hardcode-tray --apply
+    sudo /opt/numix-folders/numix-folders -p
+
+    rm -f ~/.local/share/applications/telegramdesktop.desktop
+}
