aeaf43a1988c6ea38a66ea8862a23e3b366a2912
Edgard Castro <castro@edgard.org>
2017-02-15 01:48:03 -0200

    add virtualenvwrapper autoload

--- files/zsh/env.zsh
+++ files/zsh/env.zsh
@@ -14,10 +14,17 @@ export LESS='-R'
 export EDITOR='code -w -n'
 export VISUAL='code -w -n'
 export BROWSER='chromium'
+export PROJECT_HOME=~/Documents/Projects
+
+
+# env: virtualenvwrapper
+if [[ -f /usr/bin/virtualenvwrapper.sh ]]; then
+  source /usr/bin/virtualenvwrapper.sh
+fi
 
 
 # env: go
 if hash go 2>/dev/null; then
-  export GOPATH=~/Documents/Projects/go
+  export GOPATH=$PROJECT_HOME/go
   echo $PATH | grep -q $GOPATH/bin || export PATH=$GOPATH/bin:$PATH
 fi
