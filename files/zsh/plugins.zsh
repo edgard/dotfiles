71673566c807225f3a31fb384200851f07e1b043
Edgard Castro <castro@edgard.org>
2017-03-01 22:42:58 -0300

    remove nvm from zsh

--- files/zsh/plugins.zsh
+++ files/zsh/plugins.zsh
@@ -6,7 +6,6 @@ source /usr/share/zsh/scripts/zplug/init.zsh
 zplug "zsh-users/zsh-completions"
 zplug "bobthecow/git-flow-completion"
 zplug "rupa/z", use:z.sh
-export NVM_LAZY_LOAD=true && zplug "lukechilds/zsh-nvm"
 zplug "zsh-users/zsh-autosuggestions", defer:1
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-history-substring-search", defer:3
