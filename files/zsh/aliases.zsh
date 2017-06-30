e58a5d768eb20060e1d9ca62ebc2db965b3ecc2a
Edgard Castro <castro@edgard.org>
2017-06-30 10:03:25 -0300

    add show hidden files alias

--- files/zsh/aliases.zsh
+++ files/zsh/aliases.zsh
@@ -6,3 +6,19 @@ alias history='history 1'
 alias grep='grep --color --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=.git --exclude-dir=.bzr --exclude-dir=CVS --binary-files=without-match'
 alias ls='ls -FGh'
 alias update='brew update && brew upgrade'
+
+showhiddenfiles(){
+  case "$1" in
+    on)
+        defaults write com.apple.finder AppleShowAllFiles true
+        killall Finder
+        ;;
+    off)
+        defaults write com.apple.finder AppleShowAllFiles false
+        killall Finder
+        ;;
+    *)
+        echo "Usage: $0 {on|off}"
+        ;;
+  esac
+}
