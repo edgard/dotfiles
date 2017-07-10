fa55a2d12b26e7a16266f03854a9f38e1a7ada98
Edgard Castro <castro@edgard.org>
2017-07-10 02:00:39 -0300

    add mas upgrade to update alias

--- files/zsh/aliases.zsh
+++ files/zsh/aliases.zsh
@@ -5,7 +5,7 @@ alias j='z'
 alias history='history 1'
 alias grep='grep --color --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=.git --exclude-dir=.bzr --exclude-dir=CVS --binary-files=without-match'
 alias ls='ls -FGh'
-alias update='brew update && brew upgrade'
+alias update='brew update && brew upgrade; mas upgrade'
 
 showhiddenfiles(){
   case "$1" in
