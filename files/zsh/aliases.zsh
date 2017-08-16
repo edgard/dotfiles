794c2814cbdf174cd57f0139c1d7dbcaf94a1d58
Edgard Castro <castro@edgard.org>
2017-08-16 11:20:42 -0300

    add cask update to update alias

--- files/zsh/aliases.zsh
+++ files/zsh/aliases.zsh
@@ -5,7 +5,7 @@ alias j='z'
 alias history='history 1'
 alias grep='grep --color --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=.git --exclude-dir=.bzr --exclude-dir=CVS --binary-files=without-match'
 alias ls='ls -FGh'
-alias update='brew update && brew upgrade; mas upgrade'
+alias update='brew update && brew upgrade; brew cask outdated | xargs brew cask reinstall; mas upgrade'
 
 ted(){
   open -a TextEdit $*
