1c7b136677e1ba173e290d856f08788b7684a983
Edgard Castro <castro@edgard.org>
2017-08-20 18:20:38 -0300

    remove unused aliases

--- files/zsh/aliases.zsh
+++ files/zsh/aliases.zsh
@@ -1,12 +1,7 @@
 #!/bin/zsh
 
 # aliases
-alias j='z'
 alias history='history 1'
 alias grep='grep --color --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=.git --exclude-dir=.bzr --exclude-dir=CVS --binary-files=without-match'
 alias ls='ls -FGh'
 alias update='brew update && brew upgrade; brew cask outdated | xargs brew cask reinstall; mas upgrade'
-
-ted(){
-  open -a TextEdit $*
-}
