873735c362ab2fb5060c47e7bc678019aaa61bfd
Edgard Castro <edgardcastro@gmail.com>
2018-08-03 15:12:12 +0200

    remove rbenv load

--- files/zsh/env.zsh
+++ files/zsh/env.zsh
@@ -17,6 +17,3 @@ export VISUAL='code -w -n'
 # gcloud
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
-
-# rbenv
-eval "$(rbenv init -)"
