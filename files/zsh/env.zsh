369841684ba98be481d3abd3606072616cb6f4bf
Edgard Castro <edgardcastro@gmail.com>
2020-01-27 22:02:04 -0600

    update

--- files/zsh/env.zsh
+++ files/zsh/env.zsh
@@ -2,11 +2,6 @@
 # environment
 # -----------------------------------------------------------------------------
 
-# set locale
-export LC_ALL="en_US.UTF-8"
-export LANG="en_US.UTF-8"
-export LANGUAGE="en_US.UTF-8"
-
 # history
 HISTFILE=~/.zsh_history
 HISTSIZE=500000
