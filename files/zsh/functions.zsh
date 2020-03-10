648cc39d687ef5f481afac9bbecec52c54f9eabd
Edgard Castro <edgardcastro@gmail.com>
2020-03-10 10:18:14 +0100

    re-add linux

--- files/zsh/functions.zsh
+++ files/zsh/functions.zsh
@@ -2,12 +2,17 @@
 # functions
 # -----------------------------------------------------------------------------
 
-function update() {
+[[ "$OSTYPE" == "darwin"* ]] && function update() {
     brew update
     brew upgrade
     brew cask outdated | xargs brew cask reinstall
     mas upgrade
     brew cleanup -s
+}
+
+[[ "$OSTYPE" == "linux"* ]] && function update() {
+    yay -Syyuu --needed
+    yay -Rns "$(yay -Qtdq)"
 }
 
 function setenv() {
--- files/zsh/post.zsh
+++ files/zsh/post.zsh
@@ -5,21 +5,17 @@
 # remove duplicate path entries
 PATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PATH}))')"
 
-# execute code in the background to not affect the current session
-(
-    # <https://github.com/zimfw/zimfw/blob/master/login_init.zsh>
-    setopt LOCAL_OPTIONS EXTENDED_GLOB
-    autoload -U zrecompile
-
-    # compile zcompdump, if modified, to increase startup speed.
-    zcompdump="${HOME}/.zcompdump"
-    if [[ -s "${zcompdump}" && (! -s "${zcompdump}.zwc" || "$zcompdump" -nt "${zcompdump}.zwc") ]]; then
-        zrecompile -pq "${zcompdump}"
+# zcompinit
+{
+    setopt extendedglob local_options
+    autoload -Uz compinit
+    local zcd=${HOME}/.zcompdump
+    local zcdc="${zcd}.zwc"
+    if [[ -f "${zcd}"(#qN.m+1) ]]; then
+        compinit -i -d "${zcd}"
+        { rm -f "${zcdc}" && zcompile "${zcd}" } &!
+    else
+        compinit -C -d "${zcd}"
+        { [[ ! -f "${zcdc}" || "${zcd}" -nt "${zcdc}" ]] && rm -f "${zcdc}" && zcompile "${zcd}" } &!
     fi
-    # zcompile .zshrc
-    zrecompile -pq ${HOME}/.zshrc
-    # recompile all zsh or sh
-    for f in ${HOME}/.zsh/**/*.*sh; do
-        zrecompile -pq $f
-    done
-) &!
+}
