2e44b1042249b4867f1b93a2f6fff418b2a486f8
Edgard Castro <edgardcastro@gmail.com>
2020-09-21 08:43:50 +0000

    back to bash again

--- files/zsh/aliases.zsh
+++
@@ -1,9 +0,0 @@
-# -----------------------------------------------------------------------------
-# aliases
-# -----------------------------------------------------------------------------
-
-# aliases
-alias history='history 1'
-alias grep="grep --color --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=.git --exclude-dir=.bzr --exclude-dir=CVS --binary-files=without-match"
-[[ "${OSTYPE}" == "darwin"* ]] && alias ls="ls -FGh"
-[[ "${OSTYPE}" == "linux"* ]] && alias ls="ls -FGhN --color=auto"
