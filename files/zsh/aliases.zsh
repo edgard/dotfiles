e7cf47ffe5e6a41d81dccd71c93335ffeebfeb3f
Edgard Castro <castro@edgard.org>
2021-07-12 14:39:43 +0000

    Add k8s aliases

--- files/zsh/aliases.zsh
+++ files/zsh/aliases.zsh
@@ -6,3 +6,10 @@ alias hs='history -a; history -n'
 alias grep="grep --color --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=.git --exclude-dir=.bzr --exclude-dir=CVS --binary-files=without-match"
 [[ "${OSTYPE}" == "darwin"* ]] && alias ls="ls -FGh"
 [[ "${OSTYPE}" == "linux"* ]] && alias ls="ls -FGhN --color=auto"
+
+# kubernetes
+alias k='kubectl'
+alias ksys='kubectl --namespace=kube-system'
+alias kg='kubectl get'
+alias kd='kubectl describe'
+alias kctx='kubectl config use-context'
