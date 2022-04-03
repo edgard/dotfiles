a8964cb0807efe7bfef72b1cfd40159be4f463f4
Edgard Castro <castro@edgard.org>
2022-04-03 06:38:12 +0000

    add kubernetes aliases

--- files/zsh/aliases.zsh
+++ files/zsh/aliases.zsh
@@ -2,8 +2,13 @@
 # aliases
 # -----------------------------------------------------------------------------
 
-# aliases
+# misc
 alias history='history 1'
 alias grep="grep --color --exclude-dir=.hg --exclude-dir=.svn --exclude-dir=.git --exclude-dir=.bzr --exclude-dir=CVS --binary-files=without-match"
 [[ "${OSTYPE}" == "darwin"* ]] && alias ls="ls -FGh"
 [[ "${OSTYPE}" == "linux"* ]] && alias ls="ls -FGhN --color=auto"
+
+# kubernetes
+alias k="kubectl"
+alias kns="kubens"
+alias kctx="kubectx"
