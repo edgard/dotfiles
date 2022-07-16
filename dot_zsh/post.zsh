320a6072c08b23761bf6ab5282dd45551ca22683
Edgard Castro <castro@edgard.org>
2022-07-16 07:44:24 +0200

    zsh completion cache compilation

--- dot_zsh/post.zsh
+++ dot_zsh/post.zsh
@@ -4,3 +4,19 @@
 
 # remove duplicate path entries
 PATH="$(perl -e 'print join(":", grep { not $seen{$_}++ } split(/:/, $ENV{PATH}))')"
+
+# generate completion cache
+{
+  setopt extendedglob local_options
+  autoload -Uz compinit
+  local zcd=${ZDOTDIR:-$HOME}/.zcompdump
+  local zcdc="$zcd.zwc"
+  if [[ -f "$zcd"(#qN.m+1) ]]; then
+        compinit -i -d "$zcd"
+        touch "$zcd"
+        { rm -f "$zcdc"; zcompile "$zcd" } &!
+  else
+        compinit -C -d "$zcd"
+        [[ ! -f "$zcdc" || "$zcd" -nt "$zcdc" ]] && { rm -f "$zcdc"; zcompile "$zcd" } &!
+  fi
+}
