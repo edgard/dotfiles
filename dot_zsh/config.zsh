14c8b9c5ea6f48005d604150b40f3cb6533a15f6
Edgard Castro <castro@edgard.org>
2025-03-05 10:33:47 +0100

    use unsetopt instead of no_xxxx

--- dot_zsh/config.zsh
+++ dot_zsh/config.zsh
@@ -1,35 +1,35 @@
 # Core zsh configuration
 
 # History options
-setopt append_history        # Append to history file
-setopt extended_history      # Save timestamp and duration
-setopt hist_expire_dups_first # Remove duplicates first when trimming
-setopt hist_fcntl_lock       # Use fcntl to lock history file
-setopt hist_ignore_all_dups  # Remove older duplicates
-setopt hist_reduce_blanks    # Remove superfluous blanks
-setopt hist_save_no_dups     # Don't write duplicate commands
-setopt inc_append_history    # Add commands incrementally
+setopt APPEND_HISTORY        # Append to history file
+setopt EXTENDED_HISTORY      # Save timestamp and duration
+setopt HIST_EXPIRE_DUPS_FIRST # Remove duplicates first when trimming
+setopt HIST_FCNTL_LOCK       # Use fcntl to lock history file
+setopt HIST_IGNORE_ALL_DUPS  # Remove older duplicates
+setopt HIST_REDUCE_BLANKS    # Remove superfluous blanks
+setopt HIST_SAVE_NO_DUPS     # Don't write duplicate commands
+setopt INC_APPEND_HISTORY    # Add commands incrementally
 
 # Directory options
-setopt auto_cd               # Change directory without cd command
-setopt auto_pushd            # Make cd push the old directory onto the stack
-setopt pushd_ignore_dups     # Don't push multiple copies of the same directory
-setopt pushd_minus           # Make pushd - work like pushd +
-setopt pushd_silent          # Don't print the directory stack after pushd/popd
-setopt chase_links           # Resolve symlinks when cd'ing
+setopt AUTO_CD               # Change directory without cd command
+setopt AUTO_PUSHD            # Make cd push the old directory onto the stack
+setopt PUSHD_IGNORE_DUPS     # Don't push multiple copies of the same directory
+setopt PUSHD_MINUS           # Make pushd - work like pushd +
+setopt PUSHD_SILENT          # Don't print the directory stack after pushd/popd
+setopt CHASE_LINKS           # Resolve symlinks when cd'ing
 
 # Completion options
-setopt always_to_end         # Move cursor to end on completion
-setopt auto_list             # List choices on ambiguous completion
-setopt auto_menu             # Show completion menu on tab press
-setopt complete_in_word      # Complete from both ends
-setopt no_list_beep          # Don't beep on ambiguous completion
+setopt ALWAYS_TO_END         # Move cursor to end on completion
+setopt AUTO_LIST             # List choices on ambiguous completion
+setopt AUTO_MENU             # Show completion menu on tab press
+setopt COMPLETE_IN_WORD      # Complete from both ends
+unsetopt LIST_BEEP           # Don't beep on ambiguous completion
 
 # Correction options
-setopt correct               # Try to correct spelling of commands
+setopt CORRECT               # Try to correct spelling of commands
 
 # Input/output options
-setopt interactive_comments  # Allow comments in interactive shells
-setopt no_clobber            # Don't overwrite files with >
-setopt no_flow_control       # Disable Ctrl+S/Ctrl+Q flow control
-setopt no_beep               # Disable terminal beeping
+setopt INTERACTIVE_COMMENTS  # Allow comments in interactive shells
+unsetopt CLOBBER             # Don't overwrite files with >
+unsetopt FLOW_CONTROL        # Disable Ctrl+S/Ctrl+Q flow control
+unsetopt BEEP                # Disable terminal beeping
