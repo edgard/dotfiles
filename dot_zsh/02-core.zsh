d32d9cabe20bbd9f84e5732a7930a29b801100d1
Edgard Castro <castro@edgard.org>
2025-03-07 20:06:07 +0100

    remove the use of zim environment module since we have our own

--- dot_zsh/02-core.zsh
+++ dot_zsh/02-core.zsh
@@ -16,6 +16,7 @@ setopt HIST_IGNORE_ALL_DUPS      # No duplicate entries
 setopt HIST_EXPIRE_DUPS_FIRST    # Remove duplicates first when exceeding HISTSIZE
 setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks
 setopt HIST_IGNORE_SPACE         # Don't record entries starting with space
+setopt HIST_FIND_NO_DUPS         # No duplicates in history search
 
 # Directory navigation
 setopt AUTO_CD                   # cd to dir when name is used as command
@@ -25,21 +26,23 @@ setopt PUSHD_TO_HOME             # pushd defaults to $HOME
 setopt PUSHD_MINUS               # Swap +/- operators
 setopt PUSHD_SILENT              # No stack printing
 
+# Globbing and patterns
+setopt EXTENDED_GLOB             # Extended syntax
+setopt RC_EXPAND_PARAM           # Array expansion with parameters
+unsetopt CASE_GLOB               # Case insensitive globbing
+unsetopt CASE_MATCH              # Case insensitive matching
+
 # Completion
 setopt COMPLETE_IN_WORD          # Complete from both ends
 setopt ALWAYS_TO_END             # Move cursor to end after completion
 setopt COMPLETE_ALIASES          # Complete aliases
-setopt HASH_LIST_ALL             # Hash entire command path
 setopt MAGIC_EQUAL_SUBST         # Completion after =
 
 # Tab completion
 setopt AUTO_LIST                 # List on ambiguous completion
 setopt AUTO_MENU                 # Menu after second tab
 setopt LIST_PACKED               # Compact completion list
-
-# Globbing and patterns
-setopt EXTENDED_GLOB             # Extended syntax
-setopt RC_EXPAND_PARAM           # Array expansion with parameters
+unsetopt LIST_BEEP               # No completion beep
 
 # Correction (disabled)
 unsetopt CORRECT                 # Don't correct commands
@@ -49,24 +52,21 @@ unsetopt CORRECT_ALL             # Don't correct arguments
 setopt AUTO_RESUME               # Resume by name
 setopt LONG_LIST_JOBS            # Long format
 setopt NOTIFY                    # Immediate status reporting
+unsetopt HUP                     # Don't kill background jobs
+unsetopt CHECK_JOBS              # Don't report on exit
+unsetopt BG_NICE                 # No background job priority
 
 # Shell behavior
 setopt INTERACTIVE_COMMENTS      # Allow comments
 setopt COMBINING_CHARS           # Handle multi-byte characters
 setopt MULTIOS                   # Multiple redirections
 setopt SHORT_LOOPS               # Short control structures
+setopt HASH_LIST_ALL             # Hash entire command path
 setopt HASH_CMDS                 # Hash command locations
 setopt PATH_SCRIPT               # Path expansion for scripts
-
-# Disabled for security/usability
+setopt CLOBBER                   # Allow > to overwrite files
 unsetopt BEEP                    # No error beep
-unsetopt LIST_BEEP               # No completion beep
-unsetopt CASE_GLOB               # Case insensitive globbing
-unsetopt CASE_MATCH              # Case insensitive matching
-unsetopt HUP                     # Don't kill background jobs
-unsetopt CHECK_JOBS              # Don't report on exit
 unsetopt FLOW_CONTROL            # No flow control
-unsetopt CLOBBER                 # Require >| for truncate
 unsetopt MAIL_WARNING            # No mail warnings
 
 ## KEY BINDINGS ##
