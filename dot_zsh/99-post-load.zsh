c5b0eab4828a22f84e3fefdac2c59b276b236819
Edgard Castro <castro@edgard.org>
2025-04-25 00:23:09 +0200

    refactor: move WSL2 runtime directory fix to 99-post-load.zsh and update zshrc plugins

--- dot_zsh/99-post-load.zsh
+++ dot_zsh/99-post-load.zsh
@@ -7,7 +7,24 @@
 # Refresh command hash table
 hash -r 2>/dev/null || command rehash 2>/dev/null
 
-# VSCode shell integration
+#---------------------------------------
+# WSL2 Runtime Directory Fix
+#---------------------------------------
+
+if [[ "$OSTYPE" == linux* ]]; then
+    if grep -qi microsoft /proc/version; then
+        export XDG_RUNTIME_DIR=/run/user/$(id -u)
+        if [ ! -d "$XDG_RUNTIME_DIR" ]; then
+            sudo mkdir -p $XDG_RUNTIME_DIR
+            sudo chmod 700 $XDG_RUNTIME_DIR
+            sudo chown $(id -u):$(id -g) $XDG_RUNTIME_DIR
+        fi
+    fi
+fi
+
+#---------------------------------------
+# VSCode Integration Fix
+#---------------------------------------
 if [[ "$TERM_PROGRAM" == "vscode" ]]; then
   if command -v code &>/dev/null; then
     local vscode_integration_path
