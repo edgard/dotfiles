f05db8ca8e962026108a9ebd0c73b0b82e5cf240
Edgard Castro <castro@edgard.org>
2025-09-14 15:50:32 +0200

    fix: better LSCOLORS

--- dot_zsh/02-env.zsh
+++ dot_zsh/02-env.zsh
@@ -32,7 +32,7 @@ elif (( $+commands[vi] )); then
 fi
 
 export CLICOLOR=1
-export LSCOLORS='exfxgxdxcxegedabagacad'
+export LSCOLORS='ExGxFxDxCxegedabagacad'
 
 if (( $+commands[fzf] )); then
     export FZF_DEFAULT_OPTS="--height 10 --layout=reverse --color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf --color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
